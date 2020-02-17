b68f575e29217d4c611db670f738731a14526b45
Daniel Sasser <danielbsasser@gmail.com>
2020-02-17 21:56:13 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -14,7 +14,7 @@ end
 
 def apply_discount
   if @discount == 0
-    
+    return "There is no discount to apply."
   else
     @total = @total - ((@total * @discount) / 100)
     return "After the discount, the total comes to $#{@total}."
